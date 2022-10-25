e623cee04e498551beff380b1962c9e67baecebb
Sparx27 <nicolasgcardani@gmail.com>
2022-10-25 11:57:20 -0300

    My first backend with Node

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json()) // Necesario para parsear a json el POST
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript watching changes',
+    date: '2022-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
+// const app = http.createServer((request, response) => {
+//  response.writeHead(200, { 'Content-Type': 'application/json' })
+//  response.end(JSON.stringify(notes))
+// })
+
+app.get('/', (request, response, next) => {
+  response.send('<h1>Hello World!</h1>')
+  next()
+})
+
+app.get('/api/notes', (request, response, next) => {
+  response.json(notes)
+  next()
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body
+
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: 'note.content is missing'
+    })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = [...notes, newNote]
+  res.status(201).json(newNote)
+})
+
+// PARA MANEJAR ERRORES, POR EJEMPLO UNA RUTA QUE NO ENCUENTRE
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
